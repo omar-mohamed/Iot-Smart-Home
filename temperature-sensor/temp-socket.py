83ca26c855d0b544fd46e1446d76155cbf8552be
mohammadKhalifa <moyle2010@gmail.com>
2018-04-20 11:18:55 +0000

    Added temperature code

---
+++ temperature-sensor/temp-socket.py
@@ -0,0 +1,53 @@
+import mraa
+import time
+import socket
+import json 
+
+ROOM_NO=2
+
+
+HOST='192.168.1.100' #master board IP
+PORT=5010
+
+# init socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+try:
+    sock.connect((HOST, PORT))
+except:
+    print ("Error with connection")
+
+
+yellow= mraa.Gpio(12)
+red = mraa.Gpio(13)
+
+x = mraa.Aio(0)
+
+#init led
+yellow.dir(mraa.DIR_OUT)
+red.dir(mraa.DIR_OUT)
+
+yellow.write(0)
+red.write(0)
+
+
+
+while True:
+    
+    volt= x.read() * 2.0/ 1023
+    temp = volt / 0.01
+    
+    dic={'room_no':ROOM_NO, 'temp':1 if temp > 20 else 0}
+    sock.send(json.dumps(dic))
+
+    print ("temp= %.2f C"%(temp))
+
+    if temp >= 26:
+        red.write(1)
+        yellow.write(0)
+    else :
+        red.write(0)
+        yellow.write(1)
+
+
+    time.sleep(1)
+
