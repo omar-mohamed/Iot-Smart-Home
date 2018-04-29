19c5b8164aa2984ea518cebc0e6884d814f21a78
Omar Mohamed Abdel-latif <omar.mohamed.latif@gmail.com>
2018-04-30 01:28:16 +0200

    -removed extra recieve ip and recieve port fields -added checkbox to enable sending location automatically or disable it -added button to send location manually -used a better way to get location that depends on fusion between gps and mobile network to solve indoor gps problems (Google  API)

--- Smart Home Mobile App/app/src/main/java/omar/mohamed/latif/gmail/com/smarthome/MainActivity.java
+++ Smart Home Mobile App/app/src/main/java/omar/mohamed/latif/gmail/com/smarthome/MainActivity.java
@@ -1,11 +1,9 @@
 package omar.mohamed.latif.gmail.com.smarthome;
 
 import android.Manifest;
-import android.content.Context;
+import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
@@ -13,16 +11,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
-    private EditText receiveIpEditText;
-    private EditText receivePortEditText;
+//    private EditText receiveIpEditText;
+//    private EditText receivePortEditText;
+    private FusedLocationProviderClient mFusedLocationClient;
+    private LocationRequest mLocationRequest;
+    private LocationCallback mLocationCallback;
 
     private EditText sendIpEditText;
     private EditText sendPortEditText;
@@ -31,42 +42,146 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView ipTextView;
 
-    private TextView receivedTextView;
+//    private TextView providerTextView;
 
-    private TcpClient mTcpClientReceive;
+    private TextView locationTextView;
+
+    private EditText boardNameEditText;
+
+    private CheckBox sendAutoCheckBox;
+
+//    private TextView receivedTextView;
+
+//    private TcpClient mTcpClientReceive;
     private TcpClient mTcpClientSend;
+
 
 
     private double longitude=0;
     private double latitude=0;
+    boolean mRequestingLocationUpdates;
+    private String REQUESTING_LOCATION_UPDATES_KEY="requestingLocationUpdates";
 
-    private void initLocationModule()
-    {
-        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+    private void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(10000);
+        mLocationRequest.setFastestInterval(2000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(mLocationRequest);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mRequestingLocationUpdates) {
+            startLocationUpdates();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stopLocationUpdates();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putBoolean(REQUESTING_LOCATION_UPDATES_KEY,
+                mRequestingLocationUpdates);
+        // ...
+        super.onSaveInstanceState(outState);
+    }
+
+    private void stopLocationUpdates() {
+        mFusedLocationClient.removeLocationUpdates(mLocationCallback);
+    }
+
+    private void startLocationUpdates() {
 
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this,
                     new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
-
         }
-        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 2, locationListener);
+
+
+        mFusedLocationClient.requestLocationUpdates(mLocationRequest,
+                mLocationCallback,
+                null /* Looper */);
+    }
+
+    private void initLocationCallback()
+    {
+        mLocationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                if (locationResult == null) {
+                    return;
+                }
+                List<Location> locations=locationResult.getLocations();
+                if(locations.size()>0)
+                {
+                    longitude = locations.get(0).getLongitude();
+                    latitude = locations.get(0).getLatitude();
+                    locationTextView.setText("(" + String.valueOf(longitude) + "," + String.valueOf(latitude) + ")");
+                    if (sendAutoCheckBox.isChecked()) {
+                        sendLocation();
+                    }
+                }
+
+            };
+        };
+
+    }
+
+    private void initLocationModule()
+    {
+        createLocationRequest();
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        initLocationCallback();
+        startLocationUpdates();
+
+//        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+//
+//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//            ActivityCompat.requestPermissions(this,
+//                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
+//
+//        }
+//
+//        lm.requestLocationUpdates(LocationManager.PASSIVE_PROVIDER, 2000, 2, locationListener);
 
     }
 
 
 
-    private void initListeningButton()
+    private void initConnectingButton()
     {
-        Button ListeningButton=(Button)findViewById(R.id.start_listening_button);
+        Button connectButton=(Button)findViewById(R.id.connect_button);
 
-        ListeningButton.setOnClickListener(new View.OnClickListener() {
+        connectButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 ipTextView.setText("My Ip: " + Utils.getIPAddress(true));
                 runningTextView.setVisibility(View.VISIBLE);
-                new ConnectRecvTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+//                new ConnectRecvTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
                 new ConnectSendTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
 
+            }
+        });
+    }
+
+
+    private void initSendManuallyButton()
+    {
+        Button sendButton=(Button)findViewById(R.id.send_manually_button);
+
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                sendLocation();
             }
         });
     }
@@ -75,20 +190,26 @@ public class MainActivity extends AppCompatActivity {
 
     private void initUIElements()
     {
-        receiveIpEditText=(EditText)findViewById(R.id.ip_recieve_edit_text);
-        receivePortEditText=(EditText)findViewById(R.id.receive_port_edit_text);
+//        receiveIpEditText=(EditText)findViewById(R.id.ip_recieve_edit_text);
+//        receivePortEditText=(EditText)findViewById(R.id.receive_port_edit_text);
 
         sendIpEditText=(EditText)findViewById(R.id.ip_send_edit_text);
         sendPortEditText=(EditText)findViewById(R.id.send_port_edit_text);
 
         runningTextView=(TextView) findViewById(R.id.running_text_view);
         ipTextView=(TextView) findViewById(R.id.ip_text_view);
-        receivedTextView=(TextView) findViewById(R.id.recieved_text_view);
+        locationTextView=(TextView) findViewById(R.id.location_text_view);
+//        providerTextView=(TextView) findViewById(R.id.provider_text_view);
+
+        boardNameEditText=(EditText) findViewById(R.id.board_name_edit_text);
+        sendAutoCheckBox=(CheckBox)findViewById(R.id.send_auto_checkbox);
+
+//        receivedTextView=(TextView) findViewById(R.id.recieved_text_view);
 
         ipTextView.setText("My Ip: "+ Utils.getIPAddress(true));
 
-        initListeningButton();
-
+        initConnectingButton();
+        initSendManuallyButton();
     }
 
 
@@ -97,111 +218,74 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         initLocationModule();
         initUIElements();
+        updateValuesFromBundle(savedInstanceState);
     }
 
 
-    private final LocationListener locationListener = new LocationListener() {
-
-
-        public void onLocationChanged(Location location) {
-            longitude = location.getLongitude();
-            latitude = location.getLatitude();
-
-            JSONObject obj = new JSONObject();
-            try {
-                obj.put("long", longitude);
-                obj.put("lat", latitude);
-                obj.put("source", "Board 1");
-            } catch (JSONException e) {
-                e.printStackTrace();
-                return;
-            }
-            if (mTcpClientSend != null) {
-                mTcpClientSend.sendMessage(obj.toString());
-            }
+    private void updateValuesFromBundle(Bundle savedInstanceState) {
+        if (savedInstanceState == null) {
+            return;
         }
 
-        @Override
-        public void onStatusChanged(String provider, int status, Bundle extras) {
-
+        // Update the value of mRequestingLocationUpdates from the Bundle.
+        if (savedInstanceState.keySet().contains(REQUESTING_LOCATION_UPDATES_KEY)) {
+            mRequestingLocationUpdates = savedInstanceState.getBoolean(
+                    REQUESTING_LOCATION_UPDATES_KEY);
         }
 
-        @Override
-        public void onProviderEnabled(String provider) {
+    }
 
+
+    private void sendLocation()
+    {
+        JSONObject obj = new JSONObject();
+        try {
+            obj.put("long", longitude);
+            obj.put("lat", latitude);
+            obj.put("source", boardNameEditText.getText().toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return;
         }
-
-        @Override
-        public void onProviderDisabled(String provider) {
-
-        }
-    };
-
-
-
-
-    public class ConnectRecvTask extends AsyncTask<String, String, TcpClient> {
-
-
-        private void initTCPClient()
-        {
-
-            if (mTcpClientReceive == null) {
-                //we create a TCPClient object
-                mTcpClientReceive = new TcpClient(new TcpClient.OnMessageReceived() {
-                    @Override
-                    //here the messageReceived method is implemented
-                    public void messageReceived(String from) {
-                        Log.i("MainActivity", "messageFrom: " + from);
-
-                        JSONObject obj = new JSONObject();
-                        try {
-                            obj.put("long", longitude);
-                            obj.put("lat", latitude);
-                            obj.put("source", from);
-                            receivedTextView.setText("Received Send Location From : " + from);
-                            receivedTextView.setVisibility(View.VISIBLE);
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                            return;
-                        }
-                        if (mTcpClientSend != null) {
-                            mTcpClientSend.sendMessage(obj.toString());
-                        }
-                    }
-                });
-            }
-
-
-            mTcpClientReceive.stopClient();
-            mTcpClientReceive.SERVER_IP=receiveIpEditText.getText().toString();
-            mTcpClientReceive.SERVER_PORT=Integer.parseInt(receivePortEditText.getText().toString());
-            mTcpClientReceive.run();
-
-
-        }
-
-
-        @Override
-        protected TcpClient doInBackground(String... message) {
-
-            initTCPClient();
-            return null;
-        }
-
-        @Override
-        protected void onProgressUpdate(String... values) {
-            super.onProgressUpdate(values);
-            //response received from server
-            Log.d("test", "response " + values[0]);
-            //process server response here....
-
-
+        if (mTcpClientSend != null) {
+            mTcpClientSend.sendMessage(obj.toString());
         }
     }
+
+
+//    private final LocationListener locationListener = new LocationListener() {
+//
+//
+//        public void onLocationChanged(Location location) {
+//            longitude = location.getLongitude();
+//            latitude = location.getLatitude();
+//            locationTextView.setText("("+String.valueOf(longitude)+","+String.valueOf(latitude)+")");
+//            if(sendAutoCheckBox.isChecked())
+//            {
+//                sendLocation();
+//            }
+//        }
+//
+//        @Override
+//        public void onStatusChanged(String provider, int status, Bundle extras) {
+//            providerTextView.setText(provider+" "+String.valueOf(status));
+//        }
+//
+//        @Override
+//        public void onProviderEnabled(String provider) {
+//
+//        }
+//
+//        @Override
+//        public void onProviderDisabled(String provider) {
+//
+//        }
+//    };
+
+
 
 
 
@@ -254,3 +338,77 @@ public class MainActivity extends AppCompatActivity {
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    public class ConnectRecvTask extends AsyncTask<String, String, TcpClient> {
+//
+//
+//        private void initTCPClient()
+//        {
+//
+//            if (mTcpClientReceive == null) {
+//                //we create a TCPClient object
+//                mTcpClientReceive = new TcpClient(new TcpClient.OnMessageReceived() {
+//                    @Override
+//                    //here the messageReceived method is implemented
+//                    public void messageReceived(String from) {
+//                        Log.i("MainActivity", "messageFrom: " + from);
+//
+//                        JSONObject obj = new JSONObject();
+//                        try {
+//                            obj.put("long", longitude);
+//                            obj.put("lat", latitude);
+//                            obj.put("source", from);
+//                            receivedTextView.setText("Received Send Location From : " + from);
+//                            receivedTextView.setVisibility(View.VISIBLE);
+//                        } catch (JSONException e) {
+//                            e.printStackTrace();
+//                            return;
+//                        }
+//                        if (mTcpClientSend != null) {
+//                            mTcpClientSend.sendMessage(obj.toString());
+//                        }
+//                    }
+//                });
+//            }
+//
+//
+//            mTcpClientReceive.stopClient();
+//            mTcpClientReceive.SERVER_IP=receiveIpEditText.getText().toString();
+//            mTcpClientReceive.SERVER_PORT=Integer.parseInt(receivePortEditText.getText().toString());
+//            mTcpClientReceive.run();
+//
+//
+//        }
+//
+//
+//        @Override
+//        protected TcpClient doInBackground(String... message) {
+//
+//            initTCPClient();
+//            return null;
+//        }
+//
+//        @Override
+//        protected void onProgressUpdate(String... values) {
+//            super.onProgressUpdate(values);
+//            //response received from server
+//            Log.d("test", "response " + values[0]);
+//            //process server response here....
+//
+//
+//        }
+//    }
