0662a0685435945d85e99a1b0068fc3416c17b70
Yasmin-Abdel-Latif <Yasmeen.kloub@gmail.com>
2018-05-19 17:34:12 +0200

    update Motion sensor code

--- Motion Sensor/MotionSensor.c
+++ Motion Sensor/MotionSensor.c
@@ -1,36 +1,59 @@
 #include <stdio.h>
 #include <unistd.h>
-#include "mraa.h"
-#include "mraa/aio.h"
+#include "mraa/gpio.h"
 #include <Python.h>
-#define ANALOG 0
-#define THRESHOLD 10
+#define DIGITAL_IN 4
+#define DIGITAL_OUT 7
 
 const char* return_string()
 {
-	mraa_aio_context abc;
+	mraa_gpio_context gpio_out, gpio_in;
 	uint16_t val;
-	abc = mraa_aio_init(ANALOG);
-	if (abc == NULL)
+	mraa_result_t led_result, PIR_result;
+	gpio_in = mraa_gpio_init(DIGITAL_IN);
+	gpio_out = mraa_gpio_init(DIGITAL_OUT);
+
+	if (gpio_in == NULL)
 	{
-		printf("pin %d not initiated\n", ANALOG);
-		return "Analog pin not initiated";
+		printf("pin %d not initiated\n", DIGITAL_IN);
+		return "-1";
 	}
+	if (gpio_out == NULL)
+	{
+		printf("pin %d not initiated\n", DIGITAL_OUT);
+		return "-1";
+	}
+	
+	PIR_result = mraa_gpio_dir(gpio_in, MRAA_GPIO_IN);
+	led_result = mraa_gpio_dir(gpio_out, MRAA_GPIO_OUT);
+	
+	if (PIR_result != MRAA_SUCCESS) {
+		mraa_result_print(PIR_result);
+		printf("Pin %d is not initialised correctly\n", DIGITAL_IN);
+		return "-1";
+	}
+	if (led_result != MRAA_SUCCESS) {
+		mraa_result_print(led_result);
+		printf("Pin %d is not initialised correctly\n", DIGITAL_OUT);
+		return "-1";
+	}
+	
 	while(1)
 	{
-		sleep(1);
-		val = mraa_aio_read(abc);
-		printf("Pin A%d received %hu ", ANALOG, val);
+		val = mraa_gpio_read(gpio_in);
+		
+		printf("Pin D%d received %hu \n", DIGITAL_IN, val);
 
-		if (val > THRESHOLD)
+		if (val == 1)
 		{
-				printf("GPIO1 1\n");
-				return "1";
+			printf("GPIO1 1\n");
+			return "1";
 		}
 		else
 		{
-				printf("GPIO1 0\n");
-				return "0";
+			printf("GPIO1 0\n");
+			return "0";
 		}
+		sleep(1);
 	}
 }
